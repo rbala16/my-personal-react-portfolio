18c3133c8661972d4d077a0b77cd4ce10e43441e
Rajni bala <balatamna16@gmail.com>
2023-09-16 13:53:12 -0400

    features page is created

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import Title from '../layouts/Title';
+import Card from './Card';
+import {FaBars} from 'react-icons/fa'
 
 const Features = () => {
   return (
     <section id='features'
-     className='w-full h-[800px] pb-20 flex  border-b-[1px] border-b-black'>
-    Features
+     className='w-full  py-20   border-b-[1px] border-b-black'>
+      <Title title='Features' des='What I do'/>
+    <div className='grid grid-cols-3 gap-20'>
+      <Card title='Lorem'
+      des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+      hic consequuntur eum repellendus ad.'
+      icon={<FaBars/>}/>
+        <Card title='Lorem'
+      des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+      hic consequuntur eum repellendus ad.'
+      icon={<FaBars/>}/>
+              <Card title='Lorem'
+      des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+      hic consequuntur eum repellendus ad.'
+      icon={<FaBars/>}/>
+                   <Card title='Lorem'
+      des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+      hic consequuntur eum repellendus ad.'
+      icon={<FaBars/>}/>
+                      <Card title='Lorem'
+      des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+      hic consequuntur eum repellendus ad.'
+      icon={<FaBars/>}/>
+                     <Card title='Lorem'
+      des='Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+      hic consequuntur eum repellendus ad.'
+      icon={<FaBars/>}/>
+    </div>
     </section>
   )
 }
