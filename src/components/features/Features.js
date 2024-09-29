51f539b9acd4152c3525bac03889bf1c2ad8bc38
Rajni <balatamna16@gmail.com>
2024-09-29 14:34:43 -0400

    responsive resume section

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,35 +1,49 @@
-import React from 'react';
-import Title from '../layouts/Title';
-import Card from './Card';
-import {FaBars} from 'react-icons/fa'
+import React from "react";
+import Title from "../layouts/Title";
+import Card from "./Card";
+import { FaBars } from "react-icons/fa";
 
 const Features = () => {
   return (
-    <section id='features'
-     className='w-full  py-20   border-b-[1px] border-b-black'>
-      <Title title='Features' des='What I do'/>
-    <div className='grid grid-cols-3 gap-20'>
-      <Card title='Web Application Development'
-      des='Designing and developing user interfaces using front-end technologies such as HTML, CSS, and JavaScript.Implementing interactive and responsive features using front-end frameworks like React'
-      icon={<FaBars/>}/>
-        <Card title='Back-End Development'
-      des='Working with databases, handling data storage, retrieval, and management using technologies like MySQL, MongoDB, etc'
-      icon={<FaBars/>}/>
-              <Card title='API Development'
-      des='Creating and maintaining RESTful APIs for communication between the front-end and back-end systems.Integrating third-party APIs to enhance the functionality of web applications.'
-      icon={<FaBars/>}/>
-                   <Card title='Database Management'
-      des='Designing and implementing database schemas.Writing database queries, optimizing database performance, and ensuring data integrity.'
-      icon={<FaBars/>}/>
-                      <Card title='Troubleshooting and Debugging'
-      des='Identifying and resolving bugs, performance issues, and other technical challenges.Conducting root cause analysis and implementing solutions.'
-      icon={<FaBars/>}/>
-                     <Card title='Server Configuration and Deployment'
-      des='Configuring and deploying web applications on servers.'
-      icon={<FaBars/>}/>
-    </div>
+    <section
+      id="features"
+      className="w-full  py-20   border-b-[1px] border-b-black"
+    >
+      <Title title="Features" des="What I do" />
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
+        <Card
+          title="Web Application Development"
+          des="Designing and developing user interfaces using front-end technologies such as HTML, CSS, and JavaScript.Implementing interactive and responsive features using front-end frameworks like React"
+          icon={<FaBars />}
+        />
+        <Card
+          title="Back-End Development"
+          des="Working with databases, handling data storage, retrieval, and management using technologies like MySQL, MongoDB, etc"
+          icon={<FaBars />}
+        />
+        <Card
+          title="API Development"
+          des="Creating and maintaining RESTful APIs for communication between the front-end and back-end systems.Integrating third-party APIs to enhance the functionality of web applications."
+          icon={<FaBars />}
+        />
+        <Card
+          title="Database Management"
+          des="Designing and implementing database schemas.Writing database queries, optimizing database performance, and ensuring data integrity."
+          icon={<FaBars />}
+        />
+        <Card
+          title="Troubleshooting and Debugging"
+          des="Identifying and resolving bugs, performance issues, and other technical challenges.Conducting root cause analysis and implementing solutions."
+          icon={<FaBars />}
+        />
+        <Card
+          title="Server Configuration and Deployment"
+          des="Configuring and deploying web applications on servers."
+          icon={<FaBars />}
+        />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -7,13 +7,13 @@ const Experience = () => {
     <motion.div
     initial={{ opacity: 0 }}
     animate={{ opacity: 1, transition: { duration: 0.5 } }}
-     className='w-full flex gap-20'> 
+     className='w-full flex gap-10 lgl:gap-20'> 
 <div >
-<div className='py-12 font-titleFont' >
+<div className='py-6 lgl:py-12 font-titleFont flex flex-col gap-4"' >
   <p className='text-sm text-designColor tracking-[4px]'>2017-Present</p>
-  <h2 className='text-4xl font-bodyFont'>Experience</h2>
+  <h2 className='text-3xl md:text-4xl font-bodyFont'>Experience</h2>
 </div>
-<div className='mt-14 w-full h-[1000px] border-l-[6px] border-l-black
+<div className='mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black
 border-opacity-30 flex flex-col gap-10'>
 <ResumeCard
 title="Community Health Worker"
