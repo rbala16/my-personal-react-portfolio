b2cc68c405ec0171b2ef8d6b4fe2d07629937dd7
Rajni <balatamna16@gmail.com>
2024-10-07 15:49:58 -0400

    certificates updated

---
+++ src/constants/educationData.js
@@ -0,0 +1,41 @@
+export const leftEducation = [
+  {
+    title: "Full Stack Web Developer Bootcamp (April 2023-November 2023)",
+    subTitle: "University of Toronto School of Continuing Studies",
+    result: "99/100",
+    des: "Completed immersive Full Stack Web Developer bootcamp with a focus on both front-end and back-end technologies.Proficient in HTML, CSS, and JavaScript, with expertise in React.js for building dynamic user interfaces.Developed server-side scripting skills using Node.js and Express for building robust and scalable back-end systems.Applied database management principles with MongoDB and MySQL.Deployed web applications in cloud environments such as github ,googleCloud,Heroku.Enhanced problem-solving skills and attention to detail through rigorous testing practices, including unit testing and end-to-end testing.",
+  },
+  {
+    title: "Computer Engineering Technician (2017-2018)",
+    subTitle: "Niagara College Canada",
+    result: "86/100",
+    des: "Completed a comprehensive Computer Engineering Technician program with a focus on digital electronics, microprocessor systems, computer networks, and operating systems.Developed proficiency in programming languages such as HTML,CSS,Javascript,Mysql ,etc with a solid foundation in computer architecture and organization.",
+  },
+  {
+    title: "12 Grade- Non-Medical (2014-2016)",
+    subTitle: "Government Girls Sen. Sec School",
+    result: "86/100",
+    des: "Demonstrated strong analytical and problem-solving skills through advanced mathematics courses.Excelled in physical sciences, including Physics and Chemistry, showcasing a keen interest in scientific principles.Completed non-medical coursework with a focus on computer-related skills.",
+  },
+];
+
+export const rightCertificates = [
+    {
+      title:"Build Responsive Real-World Websites with HTML and CSS",
+      subTitle:"Udemy",
+      Length:"37.5",
+      des:"Gained expertise in HTML5 and modern CSS, including Flexbox, Grid, and key concepts like the box model, positioning, and inheritance. Designed and built professional, responsive websites from scratch, following web design frameworks for accessibility and visual appeal. Developed skills in planning, sketching, coding, testing, and optimizing real-world projects, while mastering developer tools, debugging, and utilizing free design assets like images, fonts, and icons. Completed over 10 hands-on challenges to enhance and apply knowledge.",
+    },
+    {
+        title:"Web Development Fundamentals",
+        subTitle:"IBM SkillsBuild",
+        Length:"12 hours",
+        des:"Demonstrates strong knowledge of web development processes, including website development, deployment, and testing, with experience using HTML, CSS, and JavaScript in an integrated development environment (IDE). Has a conceptual understanding of building interactive websites and is familiar with the skills needed for success in various web development roles, as well as the job outlook in the field.",
+    },
+    {
+        title:"The Complete Javascript Course 2024: From Zero to Expert!",
+        subTitle:"Udemy",
+        Length:"68.5 hours",
+        des:"Acquired advanced JavaScript development skills, building 6 real-world projects and mastering core concepts like variables, functions, arrays, objects, and modern ES6+ features (arrow functions, destructuring, etc.). Gained deep understanding of object-oriented programming (OOP), complex topics like closures and higher-order functions, and asynchronous JavaScript with Promises and async/await. Experienced with modern tools like NPM, Parcel, Babel, and ES6 modules, along with hands-on practice through 50+ challenges and assignments. Confident in problem-solving, code architecture, and workflow optimization.",
+    }
+];
